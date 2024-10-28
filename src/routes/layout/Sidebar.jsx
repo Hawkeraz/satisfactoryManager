97752d5bbc358bd93f4e1df642c97484fcc2f0f2
Wesley Piauhy <92885876+Hawkeraz@users.noreply.github.com>
2024-10-27 23:49:33 -0300

    Header && Sidebar
    - Finished the Sidebar work
    - Added Component Header to work with in every page

--- src/routes/layout/Sidebar.jsx
+++ src/routes/layout/Sidebar.jsx
@@ -1,11 +1,31 @@
 import { useState } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
-import "react-pro-sidebar/dist/css/styles.css";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 import { colorTokens } from "../../theme";
+import { DataTracking, LogisticTracking, Charts } from "./sidebarMenu";
+
 import * as MaterialIcons from "@mui/icons-material";
-import userIconDefault from "../../assets/user.png"
+
+import userIconDefault from "../../assets/user.png";
+
+import "react-pro-sidebar/dist/css/styles.css";
+
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  const theme = useTheme();
+  const colors = colorTokens(theme.palette.mode);
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{ color: colors.grey[100] }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+    >
+      <Typography>{title}</Typography>
+      <Link to={to} />
+    </MenuItem>
+  );
+};
 
 const Sidebar = () => {
   const theme = useTheme();
@@ -70,20 +90,91 @@ const Sidebar = () => {
             <Box mb="25px">
               <Box display="flex" justifyContent="center" alignItems="center">
                 <img
-                 alt="profile-user"
-                 width="100px"
-                 height="100px"
-                 src={userIconDefault}
-                 style={{ cursor: "pointer", borderRadius: "50%"}}
-                 />
+                  alt="profile-user"
+                  width="100px"
+                  height="100px"
+                  src={userIconDefault}
+                  style={{ cursor: "pointer", borderRadius: "50%" }}
+                />
               </Box>
 
               <Box textAlign="center">
-                <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{m: "10px 0 0 0"}}> USERNAME </Typography>
-                <Typography variant="h5" color={colors.green[500]}> USER LEVEL </Typography>
+                <Typography
+                  variant="h2"
+                  color={colors.grey[100]}
+                  fontWeight="bold"
+                  sx={{ m: "10px 0 0 0" }}
+                >
+                  USERNAME
+                </Typography>
+                <Typography variant="h5" color={colors.green[500]}>
+                  USER LEVEL
+                </Typography>
               </Box>
             </Box>
           )}
+
+          {/* Menu Items */}
+
+          <Box paddingLeft={collapser ? undefined : "10%"}>
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0px 5px 20px" }}
+            >
+              Data
+            </Typography>
+
+            {DataTracking.map((opts) => (
+              <Item
+                title={opts.title}
+                to={opts.to}
+                icon={<opts.icon />}
+                selected={selected}
+                setSelected={setSelected}
+                key={opts.id}
+              />
+            ))}
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0px 5px 20px" }}
+            >
+              Charts
+            </Typography>
+
+            {Charts.map((opts) => (
+              <Item
+                title={opts.title}
+                to={opts.to}
+                icon={<opts.icon />}
+                selected={selected}
+                setSelected={setSelected}
+                key={opts.id}
+              />
+            ))}
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0px 5px 20px" }}
+            >
+              Tracks
+            </Typography>
+
+            {LogisticTracking.map((opts) => (
+              <Item
+                title={opts.title}
+                to={opts.to}
+                icon={<opts.icon />}
+                selected={selected}
+                setSelected={setSelected}
+                key={opts.id}
+              />
+            ))}
+
+          </Box>
         </Menu>
       </ProSidebar>
     </Box>
---
+++ src/routes/layout/sidebarMenu.js
@@ -0,0 +1,52 @@
+import * as MaterialIcons from "@mui/icons-material";
+
+export const DataTracking = [
+  {
+    title: "Dashboard",
+    to: "/dash",
+    icon: MaterialIcons.DashboardOutlined,
+    id: 1,
+  },
+  {
+    title: "Battery Switch",
+    to: "/batteries",
+    icon: MaterialIcons.Battery0BarOutlined,
+    id: 2,
+  },
+  {
+    title: "Factory Manager",
+    to: "/factories",
+    icon: MaterialIcons.FactoryOutlined,
+    id: 3,
+  },
+  {
+    title: "Warehouse",
+    to: "/warehouse",
+    icon: MaterialIcons.WarehouseOutlined,
+    id: 4,
+  },
+];
+
+export const LogisticTracking = [
+  {
+    title: "Train Tracks",
+    to: "/trains",
+    icon: MaterialIcons.TrainOutlined,
+    id: 5,
+  },
+  {
+    title: "Vehicle Tracks",
+    to: "/",
+    icon: MaterialIcons.CarRental,
+    id: 6,
+  }
+];
+
+export const Charts = [
+    {
+        title: "sample",
+        to: "/chart",
+        icon: MaterialIcons.PieChartOutlined,
+        id: 7,
+    }
+]
